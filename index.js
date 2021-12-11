54f5cdf8f94354258b6b96b93596d68321a12506
Fraser LeFevre <fraser.lefevre@gmail.com>
2021-12-10 22:24:36 -0700

    Add Google Authentication

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const googleauth = require('./authentication/googleauth')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/signin', (req, res) => res.render('pages/signin', { signinurl: googleauth.signinurl() }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
